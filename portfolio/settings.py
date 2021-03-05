68d501b88f440c810005a2735454a88e26f65c58
JesusGarcia86 <the_sampritas@hotmail.com>
2021-03-05 08:05:13 -0800

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['3.133.127.25, garcia-jesus.com, www.garcia-jesus.com']
+ALLOWED_HOSTS = ['3.133.127.25', 'garcia-jesus.com', 'www.garcia-jesus.com']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
